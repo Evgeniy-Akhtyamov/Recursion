68ae75e9df61df9651b2e5438d8f8f4340cd4e19
Evgeniy-Akhtyamov <Ahtyamove@mail.ru>
2021-04-29 17:16:16 +0400

    Добвлено нахождение факториала и возведение в степень

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -19,15 +19,39 @@ namespace Recursion
             }
         }
 
-            static void Main(string[] args)
+        static decimal Factorial(int x)
         {
-            
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+        private static int PowerUp(int N, byte pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return N * PowerUp(N, --pow);
+            }
+        }
+        static void Main(string[] args)
+        {
+
             Console.WriteLine("Напишите что-то");
             var str = Console.ReadLine();
             Console.WriteLine("Укажите глубину эха");
             var deep = int.Parse(Console.ReadLine());
-
             Echo(str, deep);
+            int num = 2;
+            Console.WriteLine(PowerUp(num, 9));
+            Console.WriteLine(Factorial(num));
 
         }
     }
